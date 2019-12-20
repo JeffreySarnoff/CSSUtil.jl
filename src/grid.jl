a358c302b345bce9b96b3d4428001d9f8a8767d6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-12-20 11:07:33 +0700

    Create grid.jl

---
+++ src/grid.jl
@@ -0,0 +1,30 @@
+"""
+    rgrid(el...)
+    Row grid multiple web components (horizontal).
+"""
+function rgrid(elems::AbstractVector)
+    nelems = length(elems)
+    container(elems)(style("display" => "grid", "grid-template-row"=>"repeat(1fr, $(nelems))"))
+end
+rgrid(xs...) = rgrid([xs...])
+
+"""
+    cgrid(el...)
+    Column grid multiple web components (vertical).
+"""
+function cgrid(elems::AbstractVector)
+    nelems = length(elems)
+    container(elems)(style("display" => "grid", "grid-template-columns"=>"repeat(1fr, $(nelems))"))
+end
+cgrid(xs...) = cgrid([xs...])
+
+"""
+    grid(el...)
+    Row and Column grid multiple web components.
+"""
+function grid(elems::AbstractMatrix)
+    nelems = length(elems)
+    nrows, ncols = size(elems)
+    container(elems)(style("display" => "grid", "grid-template-row"=>"repeat(1fr, $(nrows))",  "grid-template-column"=>"repeat(1fr, $(ncols))"))
+end
+# grid(xs...) = grid([xs...])
